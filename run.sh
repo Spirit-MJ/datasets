bf320f62b9a56a74b7d4f1f2ba195e23b303eb31
xiemingjiang <xiemingjiang@boger.local>
2025-01-13 10:22:16 +0800

    上传requirements.txt

--- run.sh
+++ run.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
-temperature=$1
-max_tokens=$2 
-time_out=$3 
-num_workers=$4 
 
+num_workers=${1:-16}    # 并发数，默认为 1
 
-# 默认设置
-[ ! -n "$temperature"  ] && temperature=0.2
-[ ! -n "$max_tokens" ] && max_tokens=20
-[ ! -n "$time_out" ] && time_out=10
-[ ! -n "$num_workers" ] && num_workers=50
-
-
-echo temperature:$temperature
-echo max_tokens:$max_tokens
-echo time_out:$time_out
 echo num_workers:$num_workers
 
-python eval.py \
-    --temperature "$temperature" \
-    --max_tokens "$max_tokens" \
-    --time_out "$time_out" \
-    --num_workers "$num_workers"
+python eval.py --num_workers $num_workers
 
 if [ $? -eq 0 ]; then
     echo "Tests completed successfully."
